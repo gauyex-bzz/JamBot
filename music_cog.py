3850cadef7415fb3a3b445ba07acb91af8f8985a
gauyex <gauyex@bzz.ch>
2023-12-05 23:57:30 +0100

    search function created

---
+++ music_cog.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+from youtube_dl import YoutubeDL
+
+
+class music_cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        self.is_playing = False
+        self.is_paused = False
+
+        self.music_queue = []
+        self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+        self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnected_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+        self.vc = None
+
+    def search_yt(self, item):
+        with YoutubeDL(self.YDL_OPTIONS) as ydl:
+            try:
+                info = ydl.extract_info('ytsearch:%s' % item, download=False)['entries'][0]
+            except Exception:
+                return False
+        return {'source': info['formats'[0]['url']], 'title': info['title']}

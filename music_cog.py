8087b9bd929854faa8fff95c29b2567a1a218c8b
Benutzer01 <gauyex@bzz.ch>
2023-12-06 13:33:24 +0100

    play next function created

--- music_cog.py
+++ music_cog.py
@@ -24,3 +24,17 @@ class music_cog(commands.Cog):
             except Exception:
                 return False
         return {'source': info['formats'[0]['url']], 'title': info['title']}
+
+    def play_next(self):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+
+            m_url = self.music_queue[0][0]['source']
+
+            self.music_queue.pop(0)
+
+            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+
+        else:
+            self.is_playing = False
+

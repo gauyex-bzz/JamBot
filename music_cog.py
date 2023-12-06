53db4c9a260bf052f1993b908e64dee815a16974
Benutzer01 <gauyex@bzz.ch>
2023-12-06 16:14:40 +0100

    play_music created

--- music_cog.py
+++ music_cog.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from youtube_dl import YoutubeDL
 
 
-class music_cog(commands.Cog):
+class Music_Cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -13,7 +13,8 @@ class music_cog(commands.Cog):
 
         self.music_queue = []
         self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
-        self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnected_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnected_streamed 1 -reconnect_delay_max 5',
+                               'options': '-vn'}
 
         self.vc = None
 
@@ -38,3 +39,20 @@ class music_cog(commands.Cog):
         else:
             self.is_playing = False
 
+    async def play_music(self, ctx):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+            m_url = self.music_queue[0][0]['source']
+
+            if self.vc is None:
+                await ctx.send('Could not connect to the VC')
+                return
+            else:
+                await self.vc.move_to(self.music_queue[0][1])
+
+            self.music_queue.pop(0)
+
+            self.vc.play(discord.FFmpegPCMAudio(m_url,  **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+
+        else:
+            self.is_playing = False

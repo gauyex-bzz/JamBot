261427b562638e63e0f9ce1a0026b02ed9ea721c
Benutzer01 <gauyex@bzz.ch>
2024-01-17 14:00:51 +0100

    stop looking at that token

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import discord
 from discord.ext import commands
 import os
+from dotenv import load_dotenv
+
 
 from help_cog import HelpCog
 from music_cog import Music_Cog
 
-bot = commands.Bot(command_prefix='/', intents=discord.Intents.default())
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 bot.remove_command('help')
 
 bot.add_cog(HelpCog(bot))
 bot.add_cog(Music_Cog(bot))
+
+load_dotenv()
 
 bot.run(os.getenv('TOKEN'))
